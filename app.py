e13e13e6e4fc18f8d1e3d85aff76ab849e0174b5
beep1000101 <matiponiatowski@o2.pl>
2025-06-22 22:16:45 +0200

    Refactor app.py to remove unused configuration retrieval

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from app_factory import create_app
-from config import get_flask_config, DevelopmentConfig
+from config import DevelopmentConfig
 
-# flask_config = get_flask_config()
-# app = create_app(flask_config)
 app = create_app(DevelopmentConfig)
 
 if __name__ == '__main__':
